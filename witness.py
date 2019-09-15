f3053e95da1172f0a884e4f601bead42a0c5957e
Anshul Sirur <vixus0@gmail.com>
2019-09-15 14:19:38 +0100

    add summary page

--- witness.py
+++ witness.py
@@ -36,24 +36,21 @@ def consent():
 @app.route('/give-statement/statement', methods=['GET', 'POST'])
 def nlp():
     if request.method == 'POST':
-        if request.form['submit'] == 'check':
-            _statement = process_text(request.form['statement'])
-            _spacy_html = displacy.render(_statement, style='ent', minify=True)
-            _hints = []
-            _added = []
+        _statement = process_text(request.form['statement'])
+        _spacy_html = displacy.render(_statement, style='ent', minify=True)
+        _hints = []
+        _added = []
 
-            for ent in _statement.ents:
-                if ent.text not in _added:
-                    if ent.label_ == 'VEHICLE':
-                        _hints.append({ 'ent': ent, 'prompt': 'more detail' })
-                        _added.append(ent.text)
-                    elif ent.label_ == 'PLACE':
-                        _hints.append({ 'ent': ent, 'prompt': 'an address' })
-                        _added.append(ent.text)
+        for ent in _statement.ents:
+            if ent.text not in _added:
+                if ent.label_ == 'VEHICLE':
+                    _hints.append({ 'ent': ent, 'prompt': 'more detail' })
+                    _added.append(ent.text)
+                elif ent.label_ == 'PLACE':
+                    _hints.append({ 'ent': ent, 'prompt': 'an address' })
+                    _added.append(ent.text)
 
-            return render_template('witness_statement.html', statement_text=_spacy_html, hints=_hints)
-        elif request.form['submit'] == 'confirm':
-            return redirect('/give-statement/summary', code=200)
+        return render_template('witness_statement.html', statement_text=_spacy_html, hints=_hints)
     return render_template('witness_statement.html')
 
 @app.route('/give-statement/summary')
