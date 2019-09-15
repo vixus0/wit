a36736ef0bf3b65a6df921345031fadc21c46ef4
Anshul Sirur <vixus0@gmail.com>
2019-09-15 11:20:55 +0100

    witness refactors

--- witness.py
+++ witness.py
@@ -12,7 +12,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def auth():
-    return render_template('auth.html')
+    return render_template('witness_auth.html', next='/give-statement')
+
+@app.route('/give-statement')
+def statement():
+    return render_template('witness_start.html', next='/give-statement/personal-info')
+
+@app.route('/give-statement/personal-info')
+def basic_info():
+    return render_template('witness_basic_info.html', next='/give-statement/care')
+
+@app.route('/give-statement/care')
+def care():
+    return render_template('witness_care.html', next='/give-statement/consent')
+
+@app.route('/give-statement/consent')
+def consent():
+    return render_template('witness_consent.html', next='/give-statement/statement')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=4000)
